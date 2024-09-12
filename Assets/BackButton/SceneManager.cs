91525ad2b1cdd525f005c1f0e1de675a67256d4e
SchaafLaura <schaaf.laura92@gmail.com>
2024-09-12 20:13:56 +0200

    docs

--- Assets/BackButton/SceneManager.cs
+++ Assets/BackButton/SceneManager.cs
@@ -5,11 +5,20 @@ using System.Linq;
 public static partial class SceneManager
 {
     static List<string> scenePaths = new();
+    /// <summary>
+    /// Uses GetTree().ChangeSceneToFile() to change the scene, but saves the path of the current one for use of back buttons
+    /// </summary>
+    /// <param name="newScenePath">The new scene to change to</param>
+    /// <param name="callingNode">The node this is getting called by, usually "this", but anything in the tree will do</param>
     public static void ChangeSceneForward(string newScenePath, Node callingNode)
     {
         scenePaths.Add(callingNode.GetTree().CurrentScene.SceneFilePath);
         callingNode.GetTree().ChangeSceneToFile(newScenePath);
     }
+    /// <summary>
+    /// Changes scene to whatever it was before. If there wasn't anything before it probably crashes, get rekt.
+    /// </summary>
+    /// <param name="callingNode">The node this is getting called by, usually "this", but anything in the tree will do</param>
     public static void ChangeSceneBackward(Node callingNode)
     {
         callingNode.GetTree().ChangeSceneToFile(scenePaths.Last());
